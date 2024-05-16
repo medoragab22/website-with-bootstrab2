d3ae061df97f52d724405e0779a2ab9ec8aca5c5
Mido Ragab <164632733+medoragab22@users.noreply.github.com>
2024-05-16 14:30:21 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+ document.getElementById("btn1").onclick=function(){
+    document.getElementById("div1").innerHTML=alert(" تم تأكيد الحجز")
+ }
+ document.getElementById("btn2").onclick=function(){
+    document.getElementById("div2").innerHTML=alert(" تم تأكيد الحجز")
+ }
+ document.getElementById("btn3").onclick=function(){
+    document.getElementById("div3").innerHTML=alert(" تم تأكيد الحجز")
+ }
+ document.getElementById("btn4").onclick=function(){
+    document.getElementById("div4").innerHTML=alert(" تم تأكيد الحجز")
+ }
